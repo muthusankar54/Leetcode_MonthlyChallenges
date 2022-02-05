30e38a94c4719703e9806824e33c24a7981d3832
Muthusankar Boothalingam <muthusankar@Muthusankars-MacBook-Pro.local>
2022-02-05 21:25:52 +0530

    Updated LeetCode Feburary 5-2022 challenge

---
+++ src/io/ms/leetcodechallenges/february2022/Merge_K_SortedList_23.java
@@ -0,0 +1,27 @@
+package io.ms.leetcodechallenges.february2022;
+
+public class Merge_K_SortedList_23 {
+
+    static class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(){
+
+        }
+
+        ListNode(int val){
+            this.val = val;
+        }
+
+        ListNode(int val,ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
