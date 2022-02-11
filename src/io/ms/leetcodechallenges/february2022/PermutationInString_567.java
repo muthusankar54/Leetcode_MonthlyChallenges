abe30a58480c3c68d1835f8fbf9d9ee781bd99e4
Muthusankar Boothalingam <muthusankar@Muthusankars-MacBook-Pro.local>
2022-02-11 22:10:47 +0530

    Updated LeetCode Feburary 11 - 2022 challenge

---
+++ src/io/ms/leetcodechallenges/february2022/PermutationInString_567.java
@@ -0,0 +1,40 @@
+package io.ms.leetcodechallenges.february2022;
+
+import java.util.*;
+
+public class PermutationInString_567 {
+
+    public static void main(String[] args) {
+
+        String s1 = "ab", s2 = "eidbaooo";
+
+        boolean result = checkInclusion(s1,s2);
+
+        System.out.println(result);
+    }
+
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        for (int i = 0; i < s1.length(); i++)
+            s1map[s1.charAt(i) - 'a']++;
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] s2map = new int[26];
+            for (int j = 0; j < s1.length(); j++) {
+                s2map[s2.charAt(i + j) - 'a']++;
+            }
+            if (matches(s1map, s2map))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+}
