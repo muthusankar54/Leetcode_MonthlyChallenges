6a2f4935fac881bd184e1e499c9c9d79eba189b1
Muthusanar <muthusankar54@gmail.com>
2022-07-28 08:22:15 +0530

    Day28 : daily challenge updated

---
+++ src/io/ms/leetcodechallenges/june2022/Day28_ValidAnagram_242.java
@@ -0,0 +1,41 @@
+package io.ms.leetcodechallenges.june2022;
+
+import java.util.HashMap;
+
+public class Day28_ValidAnagram_242 {
+
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+
+        boolean result  = isAnagram(s,t);
+
+        System.out.println(result);
+
+    }
+
+
+    public static boolean isAnagram(String s, String t) {
+
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+
+        }
+        for(char c:map.keySet()){
+            if(map.get(c)!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
